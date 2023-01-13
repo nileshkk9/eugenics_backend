fee12ad9872f69e2e88250da697c0aa4945ae811
Nilesh Kumar Pandey <nileshkk9@gmail.com>
2023-01-14 01:45:09 +0530

    t

--- src/routes/report.js
+++ src/routes/report.js
@@ -41,16 +41,12 @@ router.post("/regional-report", auth, async (req, res, next) => {
     if (req.user.level === "EMP") {
       throwError("EMP Level Not Authorized", 404);
     }
-    const s = new Date(req.body.startDate).toLocaleString(undefined, {
-      timeZone: "Asia/Kolkata",
-    });
-    const e = new Date(req.body.endDate).toLocaleString(undefined, {
-      timeZone: "Asia/Kolkata",
-    });
+    const s = new Date(req.body.startDate);
+    const e = new Date(req.body.endDate);
     const data = await reportService.getAllEntriesByUser(
       req.body.username,
-      `${s.split("/")[2].split(",")[0]}-${s.split("/")[0]}-${s.split("/")[1]}`,
-      `${e.split("/")[2].split(",")[0]}-${e.split("/")[0]}-${e.split("/")[1]}`
+      `${s.getFullYear()}-${s.getMonth() + 1}-${s.getDate()}`,
+      `${e.getFullYear()}-${e.getMonth() + 1}-${e.getDate()}`
     );
 
     res.send(data);
