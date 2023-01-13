5a168d38eca7110507f8be76377b9e4a7fd2aa92
Nilesh Kumar Pandey <nileshkk9@gmail.com>
2023-01-14 01:20:52 +0530

    dateDtrings

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -6,22 +6,21 @@ const connection = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  connectTimeout  : 3 * 60 * 60 * 1000,
-  timeout  : 3 * 60 * 60 * 1000,
+  connectTimeout: 3 * 60 * 60 * 1000,
+  timeout: 3 * 60 * 60 * 1000,
+  dateStrings: true,
 });
-
 
 const connectionOld = mysql.createPool({
   host: process.env.HOST,
   user: process.env.DB_USER_OLD,
   password: process.env.DB_PASS_OLD,
   database: process.env.DB_NAME_OLD,
-  connectTimeout  : 3 * 60 * 60 * 1000,
-  timeout  : 3 * 60 * 60 * 1000,
+  connectTimeout: 3 * 60 * 60 * 1000,
+  timeout: 3 * 60 * 60 * 1000,
 });
-
 
 const query = util.promisify(connection.query).bind(connection);
 const queryOld = util.promisify(connectionOld.query).bind(connectionOld);
 
-module.exports = { connection,connectionOld, query, queryOld };
+module.exports = { connection, connectionOld, query, queryOld };
