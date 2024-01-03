2859bdec0e01f6f0d17a68126cb779a27e885bac
Nilesh Kumar Pandey <nileshkk9@gmail.com>
2024-01-04 03:55:53 +0530

    minor bug fix

--- src/services/assets.js
+++ src/services/assets.js
@@ -7,7 +7,7 @@ assets.getCarouselImages = async () => {
   return res;
 };
 assets.getProductDetails = async () => {
-  const sql = 'SELECT * from products WHERE isACtive = 1';
+  const sql = 'SELECT * from products WHERE isActive = 1';
   const res = await queryWebsite(sql);
   return res;
 };
